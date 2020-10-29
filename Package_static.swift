0c20c204289de306d8b3247142759ac6d42f29df
Sven Roeder <svenroed@gmail.com>
2020-10-29 18:38:17 +0100

    switch to dynamic framework for Package.swift and keep static version for further inspection

--- Package_static.swift
+++ Package_static.swift
@@ -8,18 +8,25 @@ let package = Package(
         .iOS(.v9)	
     ],	
     products: [	
-        // Products define the executables and libraries a package produces, and make them visible to other packages.	
-        .library(	
-            name: "GLGeoRealtime",	
-            targets: ["GLGeoRealtime"])	
+    	.library(name: "GLGeoRealtime", type: .static, targets: ["GLGeoRealtime"])
     ],	
     dependencies: [	
         // Dependencies declare other packages that this package depends on.	
     ],	
     targets: [	
         .binaryTarget(	
-            name: "GLGeoRealtime",	
+            name: "GLGeoRealtimeLibrary",	
             path: "GLGeoRealtimeStatic.xcframework"	
-        )	
+        ),
+    .target(
+      name: "GLGeoRealtime",
+      dependencies: ["GLGeoRealtimeLibrary"],
+      path: "GLGeoRealtime",
+
+      publicHeadersPath: "headers/GLGeoRealtimeLibrary",
+      linkerSettings: [
+        .linkedLibrary("z"),
+      ]
+    ),
     ]	
 ) 

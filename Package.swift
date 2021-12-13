93b41a7aeffa743da3159417c2f23247fd629d48
Sven Roeder <svenroed@gmail.com>
2021-12-13 20:19:29 +0100

    static package test

--- Package.swift
+++ Package.swift
@@ -1,25 +1,31 @@
 // swift-tools-version:5.3
 import PackageDescription
-
-let package = Package(	
-    name: "GLGeoRealtime",	
+let package = Package(  
+    name: "GLGeoRealtime",    
     defaultLocalization: "en",
-    platforms: [	
-        .iOS(.v9)	
-    ],	
-    products: [	
-        // Products define the executables and libraries a package produces, and make them visible to other packages.	
-        .library(	
-            name: "GLGeoRealtime",	
-            targets: ["GLGeoRealtime"])	
-    ],	
-    dependencies: [	
-        // Dependencies declare other packages that this package depends on.	
-    ],	
-    targets: [	
-        .binaryTarget(	
-            name: "GLGeoRealtime",	
-            path: "GLGeoRealtimeStatic.xcframework"	
-        )	
-    ]	
+    platforms: [    
+        .iOS(.v9)   
+    ],  
+    products: [ 
+        // Products define the executables and libraries a package produces, and make them visible to other packages.   
+        .library(   
+            name: "GLGeoRealtime",    
+            targets: ["GLGeoRealtime"]),
+        .library(   
+            name: "Glimr",    
+            targets: ["Glimr"])   
+    ],  
+    dependencies: [ 
+        // Dependencies declare other packages that this package depends on.    
+    ],  
+    targets: [  
+        .binaryTarget(  
+            name: "GLGeoRealtime",    
+            path: "GLGeoRealtimeStatic.xcframework"   
+        ),
+        .binaryTarget(  
+            name: "Glimr",    
+            path: "Glimr.xcframework" 
+        )       
+    ]   
 ) 
